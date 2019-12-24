5af084fcedd83457bfa7367c271dd59f6646cfb7
amal <amalzali95@yahoo.com>
2019-12-24 02:46:29 +0200

    select max 6 images

--- lib/request.dart
+++ lib/request.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:multi_image_picker/multi_image_picker.dart';
 class Request extends StatefulWidget {
   @override
   _RequestState createState() => _RequestState();
 }
 
 class _RequestState extends State<Request> {
-  static TextEditingController RequestEditingContrller = TextEditingController();
-  static TextEditingController RequestDateEditingContrller = TextEditingController();
-  static TextEditingController RequestDescriptionEditingContrller = TextEditingController();
+  List<Asset> images = List<Asset>();
+  static TextEditingController RequestEditingContrller =
+      TextEditingController();
+  static TextEditingController RequestDateEditingContrller =
+      TextEditingController();
+  static TextEditingController RequestDescriptionEditingContrller =
+      TextEditingController();
 
-  static String  Request = RequestEditingContrller.toString();
-  static String  RequestDate  = RequestDateEditingContrller.toString();
-  static String  RequestDescription  = RequestDescriptionEditingContrller.toString();
+  static String Request = RequestEditingContrller.toString();
+  static String RequestDate = RequestDateEditingContrller.toString();
+  static String RequestDescription =
+      RequestDescriptionEditingContrller.toString();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        Asset asset = images[index];
+        return AssetThumb(
+          asset: asset,
+          width: 300,
+          height: 300,
+        );
+      }),
+    );
+  }
+  Future<void> loadAssets() async {
+    List<Asset> resultList = List<Asset>();
+    try {
+      resultList = await MultiImagePicker.pickImages(
+          maxImages: 5,
+          enableCamera: true,
+          selectedAssets: images,
+          cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+          materialOptions: MaterialOptions(
+            actionBarColor: "#abcdef",
+            actionBarTitle: "Example App",
+            allViewTitle: "All Photos",
+            selectCircleStrokeColor: "#000000",
+          ));
+    }
+    on Exception catch (e) {
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +76,7 @@ class _RequestState extends State<Request> {
             child: Center(
               child: Column(
                 children: <Widget>[
+
                   SizedBox(
                     height: 40,
                   ),
@@ -59,7 +106,7 @@ class _RequestState extends State<Request> {
                     autofocus: false,
                     obscureText: false,
                     keyboardType: TextInputType.datetime,
-                    controller:RequestDateEditingContrller,
+                    controller: RequestDateEditingContrller,
                     decoration: InputDecoration(
                         labelText: "Request Date",
                         hintText: "Request Date",
@@ -77,6 +124,7 @@ class _RequestState extends State<Request> {
                   SizedBox(
                     height: 10,
                   ),
+
                   TextField(
                     autofocus: false,
                     obscureText: true,
@@ -95,6 +143,23 @@ class _RequestState extends State<Request> {
                                 width: 1,
                                 color: Colors.green,
                                 style: BorderStyle.solid))),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ButtonTheme(
+                    //elevation: 4,
+                    //color: Colors.green,
+                    minWidth: double.infinity,
+                    child: MaterialButton(
+                      onPressed: () => {
+                        loadAssets()
+                      },
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      height: 40,
+                      child: Text("Select Iamges"),
+                    ),
                   ),
                   SizedBox(
                     height: 10,
